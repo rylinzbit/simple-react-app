c556cce49644ca4fea9396370acf91a339276984
Rylin Zbitowsky <rylinzbitowsky@gmail.com>
2018-10-29 09:37:10 -0700

    App config

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,10 @@ module.exports = {
       {
         test: /\.scss$/,
         loaders: ["style-loader", "css-loader", "sass-loader"]
-      }
+      },
+      {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: 'url-loader?limit=100000' }
     ]
   }
 }
